b627d5ad71c3652f4c63e63518889498628119ff
Пользователь <nastya.svirid@mail.ru>
2024-04-05 15:24:29 +0300

    create app

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../../widgets/Header"
+
+
+function Home() {
+
+  
+    return (
+        <Header></Header>
+    )
+  }
+  export default (Home)
---
+++ src/widgets/Header/Header.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+
+function Header() {
+   
+  
+    return (
+        <div>
+            <div >Logo</div>
+  
+        </div>
+    )
+  }
+  export default (Header)

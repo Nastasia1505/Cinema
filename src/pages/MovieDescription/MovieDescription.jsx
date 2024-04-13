1186eefd7a310ca4f8ca60d6a65aeed8e22c0be9
unknown <nastya.svirid@mail.ru>
2024-04-13 21:02:27 +0300

    add description

--- src/pages/MovieDescription/MovieDescription.jsx
+++ src/pages/MovieDescription/MovieDescription.jsx
@@ -3,19 +3,27 @@ import styles from "./styles.module.scss";
 import { IoChevronDownCircleOutline } from "react-icons/io5";
 import { PiTimerThin } from "react-icons/pi";
 import { MdLocalMovies } from "react-icons/md";
+import { BsTag } from "react-icons/bs";
+import { BsStopwatch } from "react-icons/bs";
+
+import duna from './Duna.jpg'
 
 function MovieDescription() {
   return (
-<div>
-    <img></img>
-    <div>
-        <h1>
+    <div className={styles.wrapper}>
+      <div className={styles.section}>
+        <img className={styles.poster} src={duna} alt="Duna" ></img>
+        <div className={styles.description}>
+          <h1 className={styles.title}>
+            Duna
+          </h1>
+          <div> <BsTag />Боевик, Драма, Фантастика</div>
+          <div> <BsStopwatch />166 мин.</div>
+          <div><p>Duke Paul Atreides joins the Fremen to become Muad'Dib while trying to stop the advance of war.</p></div>
+        </div>
+      </div>
 
-        </h1>
-        <div>genre</div>
-        <div>time</div>
-        <div><p>Description</p></div>
-        <div className={styles.options}>
+      <div className={styles.options}>
         <button className={styles.optionsBtn}>
           <LiaCalendar className={styles.optionsBtnIcon} />
           <div className={styles.optionsBtnText}>Date</div>
@@ -31,9 +39,9 @@ function MovieDescription() {
           <div className={styles.optionsBtnText}>Other</div>
           <IoChevronDownCircleOutline className={styles.optionsBtnArrow} />
         </button>
+
       </div>
     </div>
-</div>
   )
 }
 export default MovieDescription;
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -3,12 +3,17 @@ import { PiTimerThin } from "react-icons/pi";
 import { MdLocalMovies } from "react-icons/md";
 import { IoChevronDownCircleOutline } from "react-icons/io5";
 import { MovieCard } from "./ui";
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 import styles from "./styles.module.scss";
+import { style } from "@mui/system";
 
 function Movies() {
   return (
-    <div>
+    <div className={style.section}>
       <div className={styles.headerAfisha}>
         <h1 className={styles.h1}>Movies</h1>
         <div className={styles.headerAfishaBtn}>
@@ -20,7 +25,8 @@ function Movies() {
         <button className={styles.optionsBtn}>
           <LiaCalendar className={styles.optionsBtnIcon} />
           <div className={styles.optionsBtnText}>Date</div>
-          <IoChevronDownCircleOutline className={styles.optionsBtnArrow} />
+          <IoChevronDownCircleOutline className={styles.optionsBtnArrow}
+          />
         </button>
         <button className={styles.optionsBtn}>
           <PiTimerThin className={styles.optionsBtnIcon} />
